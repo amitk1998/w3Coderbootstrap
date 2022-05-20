174e906db18848dc9131d14f8af9898e4523e306
amitk1998 <105691235+amitk1998@users.noreply.github.com>
2022-05-20 14:15:55 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,91 @@
+
+
+
+$('document').ready(function()
+{
+
+$('#more1').hide();
+
+    
+    $('.myBtn1').on(
+        
+        {   
+            
+            click:function()
+            {
+                $('#more1').slideToggle(1000,function()
+                {
+                    console.log("Text has been shown successfully",this);
+                });
+                
+            }
+            
+            
+        });
+
+
+        $('#more2').hide();
+
+    
+        $('.myBtn2').on(
+            
+            {   
+                
+                click:function()
+                {
+                    $('#more2').slideToggle(1000,function()
+                    {
+                        console.log("Text has been shown successfully",this);
+                    });
+                    
+                }
+                
+                
+            });
+
+
+            $('#more3').hide();
+
+    
+    $('.myBtn3').on(
+        
+        {   
+            
+            click:function()
+            {
+                $('#more3').slideToggle(1000,function()
+                {
+                    console.log("Text has been shown successfully",this);
+                });
+                
+            }
+            
+            
+        });
+
+
+        $('#more4').hide();
+
+    
+    $('.myBtn4').on(
+        
+        {   
+            
+            click:function()
+            {
+                $('#more4').slideToggle(1000,function()
+                {
+                    console.log("Text has been shown successfully",this);
+                });
+                
+            }
+            
+            
+        });
+
+
+       
+            
+  
+        
+    })
