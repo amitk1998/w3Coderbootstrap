7d81210827a1dc9bd5d75687174c6ecbbfd0eb5f
amitk1998 <105691235+amitk1998@users.noreply.github.com>
2022-05-20 12:04:06 +0530

    Update jQueryshow.js

--- jQueryshow.js
+++ jQueryshow.js
@@ -1,161 +1,161 @@
 
-$(document).ready(function () {
+// $(document).ready(function () {
     
-//.....................Continue Reading 1 starts from here............................!
+// //.....................Continue Reading 1 starts from here............................!
 
-//This below fun hold the hide untill user clicks on "Continue Reading " Button
-   $("#showme1").hide();
+// //This below fun hold the hide untill user clicks on "Continue Reading " Button
+//    $("#showme1").hide();
     
     
-//When user clicks on button of class '.shown1' then show method will callback 
-//And it will show the holded contents by Id '#showme1' 
+// //When user clicks on button of class '.shown1' then show method will callback 
+// //And it will show the holded contents by Id '#showme1' 
 
-    $('.shown1').on(
+//     $('.shown1').on(
 
-     {   
+//      {   
     
-    click:function()
-{
-    $('#showme1').show(1500,function()
-    {
-        console.log("Text has been shown successfully",this);
-    });
+//     click:function()
+// {
+//     $('#showme1').show(1500,function()
+//     {
+//         console.log("Text has been shown successfully",this);
+//     });
     
-}
+// }
 
 
-});
+// });
 
-//Untill user leave mouse from Id '#showme1' it will not hide the content of Id
-//But when user leave mouse from Id '#showme1' then it will hide the content 
+// //Untill user leave mouse from Id '#showme1' it will not hide the content of Id
+// //But when user leave mouse from Id '#showme1' then it will hide the content 
 
-$('.shown1').on(
+// $('.shown1').on(
 
-    {   
+//     {   
    
 
-        mouseenter:function()
-        {
-          $('#showme1').hide(1500,function()
-          {
-          console.log("Text has been hidden successfully",this);
+//         mouseenter:function()
+//         {
+//           $('#showme1').hide(1500,function()
+//           {
+//           console.log("Text has been hidden successfully",this);
 
-          }
+//           }
           
-          );
+//           );
          
-        }
-    });
+//         }
+//     });
 
-//.......................Continue Reading 1 ends here!.............................!
+// //.......................Continue Reading 1 ends here!.............................!
 
 
-//.....................Continue Reading 2 starts from here.........................!
+// //.....................Continue Reading 2 starts from here.........................!
 
-  //This below fun hold the hide untill user clicks on "Continue Reading " Button
-  $("#showme2").hide();
+//   //This below fun hold the hide untill user clicks on "Continue Reading " Button
+//   $("#showme2").hide();
     
     
-//When user enter mouse on button of there class '.shown2' then show method will...
-//...callback And it will show the holded contents by Id '#showme2'  
+// //When user enter mouse on button of there class '.shown2' then show method will...
+// //...callback And it will show the holded contents by Id '#showme2'  
 
-  $('.shown2').on(
+//   $('.shown2').on(
 
-   {   
+//    {   
   
-  click:function()
-{
-  $('#showme2').show(1500,function()
-  {
-      console.log("Text has been shown successfully",this);
-  });
+//   click:function()
+// {
+//   $('#showme2').show(1500,function()
+//   {
+//       console.log("Text has been shown successfully",this);
+//   });
   
-}
+// }
 
 
-});
+// });
 
 
 
-//Untill user leave mouse from Id '#showme2' it will not hide the content of Id
-//But when user leave mouse from Id '#showme2' then it will hide the content 
+// //Untill user leave mouse from Id '#showme2' it will not hide the content of Id
+// //But when user leave mouse from Id '#showme2' then it will hide the content 
 
-$('.shown2').on(
+// $('.shown2').on(
 
-  {   
+//   {   
  
 
-        mouseenter:function()
-      {
-        $('#showme2').hide(1500,function()
-        {
-        console.log("Text has been hidden successfully",this);
+//         mouseenter:function()
+//       {
+//         $('#showme2').hide(1500,function()
+//         {
+//         console.log("Text has been hidden successfully",this);
 
-        }
+//         }
         
-        );
+//         );
        
-      }
-  });
+//       }
+//   });
 
-//......................Continue Reading 2 ends here!.................................!
-
-
+// //......................Continue Reading 2 ends here!.................................!
 
 
-//.....................Continue Reading 3 starts from here............................!
 
-//This below fun hold the hide untill user clicks on "Continue Reading " Button
-$("#showme3").hide();
+
+// //.....................Continue Reading 3 starts from here............................!
+
+// //This below fun hold the hide untill user clicks on "Continue Reading " Button
+// $("#showme3").hide();
     
     
-//When user clicks on button of class '.shown3' then show method will callback 
-//And it will show the holded contents by Id '#showme3'
+// //When user clicks on button of class '.shown3' then show method will callback 
+// //And it will show the holded contents by Id '#showme3'
 
-  $('.shown3').on(
+//   $('.shown3').on(
 
-   {   
+//    {   
   
-  click:function()
-{
-  $('#showme3').show(1500,function()
-  {
-      console.log("Text has been shown successfully",this);
-  });
+//   click:function()
+// {
+//   $('#showme3').show(1500,function()
+//   {
+//       console.log("Text has been shown successfully",this);
+//   });
   
-}
+// }
 
 
-});
+// });
 
 
-//When user enter mouse on button of there class '.shown3' then show method will...
-//...callback And it will hide the holded contents by Id '#showme3'  
+// //When user enter mouse on button of there class '.shown3' then show method will...
+// //...callback And it will hide the holded contents by Id '#showme3'  
 
-$('.shown3').on(
+// $('.shown3').on(
 
-  {   
+//   {   
  
 
-        mouseenter:function()
-      {
-        $('#showme3').hide(1500,function()
-        { 
-        console.log("Text has been hidden successfully",this);
+//         mouseenter:function()
+//       {
+//         $('#showme3').hide(1500,function()
+//         { 
+//         console.log("Text has been hidden successfully",this);
 
-        }
+//         }
         
-        );
+//         );
        
-      }
+//       }
 
     
 
 
 
-  });
+//   });
 
-//.........................Continue Reading 3 ends here.............................!
+// //.........................Continue Reading 3 ends here.............................!
 
 
-});
+// });
