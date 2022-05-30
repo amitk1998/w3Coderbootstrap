c326587ca2d2fda1a8b2adc35c6951913e040a0f
amitk1998 <105691235+amitk1998@users.noreply.github.com>
2022-05-30 13:15:18 +0530

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,22 +1,21 @@
 
 
 
-
-
-const btn = document.getElementById('dark');
-
-btn.addEventListener('click', function onClick(event) {
-
     function darkmodeFun() 
 
 {
    
 var element = document.body;
-   
 element.classList.toggle("dark-mode");
 
-}  
+} 
 
+const btn = document.getElementById('dark');
+
+btn.addEventListener('click', function onClick(event) {
+
+
+ 
   const backgroundColor = btn.style.backgroundColor;
 
   if (backgroundColor === 'white') {
