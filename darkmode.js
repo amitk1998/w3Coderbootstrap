bf2c54abff7cfb9f7f79cd4cdc70f81451eff043
amitk1998 <105691235+amitk1998@users.noreply.github.com>
2022-05-30 13:03:43 +0530

    Add files via upload

---
+++ darkmode.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+const btn = document.getElementById('dark');
+
+btn.addEventListener('click', function onClick(event) {
+
+    function darkmodeFun() 
+
+{
+   
+var element = document.body;
+   
+element.classList.toggle("dark-mode");
+
+}  
+
+  const backgroundColor = btn.style.backgroundColor;
+
+  if (backgroundColor === 'white') {
+    btn.style.backgroundColor = 'black';
+
+  
+  } else {
+    btn.style.backgroundColor = 'white';
+
+
+  }
+});
+
