dac48c474c313bd599be163a0e994396de876236
amitk1998 <105691235+amitk1998@users.noreply.github.com>
2022-05-30 13:03:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,29 +5,6 @@
 
 
  
-  const btn = document.getElementById('dark');
-
-btn.addEventListener('click', function onClick(event) {
- 
-     function darkmodeFun() 
-{
-   var element = document.body;
-   element.classList.toggle("dark-mode");
-}  
-  const backgroundColor = btn.style.backgroundColor;
-   
-
-
-  if (backgroundColor ===white') {
-    btn.style.backgroundColor = 'black';
-
-  } else {
-    btn.style.backgroundColor = 'white';
-
-  }
-});
-    
- 
 
 $('document').ready(function()
  { 
